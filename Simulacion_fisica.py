d29b89d6f0417e54e6bb1bfe28066291a525ed09
glpcc <gonzalolopecc@gmail.com>
2020-10-01 16:19:54 +0200

    el orden de los calculos importa para la conservacion de la energia

---
+++ Simulacion_fisica.py
@@ -0,0 +1,48 @@
+import pygame
+from pygame.locals import *
+import math
+
+canvas = pygame.display.set_mode((1000,1000))
+pygame.init()
+runnig = True
+
+aceleracion_gravitatoria = 10
+radio = 300
+desfase = 0 # rad
+velocidad_inicial = 0 # rad/s
+masa = 1 # kg
+
+class pendulo():
+    def __init__(self,radio,desfase,velocidad_inicial,masa):
+        self.radio = radio
+        self.angulo = desfase
+        self.velocidad_angular = velocidad_inicial
+        self.acceleracion = 0
+        self.masa = masa
+    def update(self):
+        self.fuerza_tangencial = math.cos(-self.angulo)*(aceleracion_gravitatoria*self.masa)
+        self.acceleracion = self.fuerza_tangencial/self.masa
+        self.velocidad_angular += (self.acceleracion/radio)
+        self.angulo = self.angulo - self.velocidad_angular/1000 # el 10000 es para ajustar la velocidad
+        if self.angulo > 0:
+            print(self.angulo)
+        #self.fuerza_tangencial -= self.velocidad_angular*0.0008   #El 0.0008 es una "resistencia" para paliar el fallo de el calculo
+    def draw(self,canvas):
+        pygame.draw.circle(canvas,(255,255,255),(500,500),(self.radio))
+        pygame.draw.circle(canvas,(255,0,255),(int(math.cos(self.angulo)*self.radio)+500,int(500-math.sin(self.angulo)*self.radio)),(50))
+
+Pendulo = pendulo(radio,desfase,velocidad_inicial,masa)
+
+while runnig:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            runnig = False
+    
+    canvas.fill((0,0,0))
+            
+    Pendulo.update()
+    Pendulo.draw(canvas)
+    pygame.display.flip()
+
+pygame.quit()
+        
